1de5e3a4bfa30f99c24f9d4acd778f25aefea174
John P. Murray <jpm7003@rit.edu>
2018-01-29 18:06:35 -0500

    initial commit

---
+++ unittests/edu/ncsu/csc326/coffeemaker/AllTests.java
@@ -0,0 +1,18 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(InventoryTest.class);
+		suite.addTestSuite(RecipeBookTest.class);
+		suite.addTestSuite(RecipeTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
