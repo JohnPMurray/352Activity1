2b75943e206c5dc5d9a6db6997710850f4abfa51
akn4743 <akn4743@rit.edu>
2018-02-03 17:01:35 -0500

    separated test initial recipe into 6 methods. Added toString test case and one hashcode test case

--- CoffeeMaker_JUnit_starter/unittests/edu/ncsu/csc326/coffeemaker/RecipeTest.java
+++ CoffeeMaker_JUnit_starter/unittests/edu/ncsu/csc326/coffeemaker/RecipeTest.java
@@ -1,18 +1,22 @@
 package edu.ncsu.csc326.coffeemaker;
 
 
+import org.junit.Assert;
+
 import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
 
 public class RecipeTest extends TestCase {
 	
 	private String name;
+	private String empty;
     private int price;
     private int amtCoffee;
     private int amtMilk;
     private int amtSugar;
     private int amtChocolate;
     Recipe Rec = new Recipe();
+    Recipe Rec2 = new Recipe();
     
     protected void setUp() {
     	this.name = Rec.getName();
@@ -32,12 +36,27 @@ public class RecipeTest extends TestCase {
     	this.amtChocolate = 0;
     }
 
-    public void testintialRecipe() {
+    public void testintialRecipe_001() {
     	assertEquals("Name is a empty string ", "", name);
+    }
+    
+    public void testintialRecipe_002() {
     	assertEquals("Price is 0 ", 0, price);
+    }
+    
+    public void testintialRecipe_003() {
     	assertEquals("Price is 0 ", 0, amtCoffee);
-    	assertEquals("Price is 0 ", 0, amtMilk);
+    }
+    
+    public void testintialRecipe_004() {
     	assertEquals("Price is 0 ", 0, amtSugar);
+    }
+    
+    public void testintialRecipe_005() {
+    	assertEquals("Price is 0 ", 0, amtMilk);
+    }
+    
+    public void testintialRecipe_006() {
     	assertEquals("Price is 0 ", 0, amtChocolate);
     }
     
@@ -124,5 +143,11 @@ public class RecipeTest extends TestCase {
     	catch(RecipeException success) {    		
     	}
     }
-    
+    public void testtoString() {
+    	empty = "";
+    	assertEquals(Rec.toString(),empty);
+    }
+    public void testHashcode_001() {
+    	assertTrue(Rec.hashCode() == Rec2.hashCode());
+    }
 }
