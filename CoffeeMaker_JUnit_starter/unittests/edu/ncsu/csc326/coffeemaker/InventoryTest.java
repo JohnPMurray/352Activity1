cf04efb5aeea505d2bcfcddc8b2311f7e21c7135
jes6494 <jes6494@rit.edu>
2018-02-03 15:44:59 -0500

    commite

--- CoffeeMaker_JUnit_starter/unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ CoffeeMaker_JUnit_starter/unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -1,7 +1,120 @@
 package edu.ncsu.csc326.coffeemaker;
 
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
 import junit.framework.TestCase;
 
 public class InventoryTest extends TestCase {
+	
+    private int coffee;
+    private int milk;
+    private int sugar;
+    private int chocolate;
+    Inventory inv = new Inventory();
+    Recipe Rec = new Recipe();
+    
+    protected void setUp() {
+    	this.coffee = inv.getCoffee();
+    	this.milk = inv.getMilk();
+    	this.sugar = inv.getSugar();
+    	this.chocolate = 15;
+    	
+    }
+	
+    protected void tearDown() {
+    	this.coffee = 0;
+    	this.milk = 0;
+    	this.sugar = 0;
+    	this.chocolate = 0; 
+    	//inv = new Inventory();
+    }
+    
+    public void testintialRecipe() {
+    	inv.setChocolate(0);
+    	
+    	assertEquals("Chocolate is 0 ", 0, inv.getChocolate());
+    	inv.setChocolate(-1);
+    	assertEquals("Chocolate is not negative ", 0, inv.getChocolate());
+    }
+    
+    public void testsetChocolate() {
+    	inv.setChocolate(0);
+    	
+    	assertEquals("Chocolate is 0 ", 0, inv.getChocolate());
+    	inv.setChocolate(1);
+    	assertEquals("Chocolate is 1 ", 1, inv.getChocolate());
+    	inv.setChocolate(-1);
+    	assertEquals("Chocolate is still 1 ", 1, inv.getChocolate());
+    }
+    
+    public void testsetCoffee() {
+    	inv.setCoffee(0);   	
+    	assertEquals("Coffee is 0 ", 0, inv.getCoffee());
+    	inv.setCoffee(1);
+    	assertEquals("Coffee is added ", 1, inv.getCoffee());
+    	inv.setCoffee(-1);
+    	assertEquals("Negative coffee is not added ", 1, inv.getCoffee());
+    }
+    
+    public void testsetMilk() {
+    	inv.setMilk(0);   	
+    	assertEquals("Milk is 0 ", 0, inv.getMilk());
+    	inv.setMilk(1);
+    	assertEquals("Milk is added ", 1, inv.getMilk());
+     	inv.setMilk(-1);
+    	assertEquals("Negative milk is not added ", 1, inv.getMilk());
+    }
+    
+    public void testsetSugar() {
+    	inv.setSugar(0);   	
+    	assertEquals("Sugar is 0 ", 0, inv.getSugar());
+    	inv.setSugar(1);
+    	assertEquals("Sugar is added ", 1, inv.getSugar());
+    	inv.setSugar(-1);
+    	assertEquals("Negative sugar is not added ", 1, inv.getSugar());
+    }
+    
+    public void testaddChocolate() throws InventoryException {
+    	inv.addChocolate("0");
+    	assertEquals("Chocolate is 15 ", 15, inv.getChocolate());
+    	inv.addChocolate("-1");
+    	assertEquals("Negative chocolate is not added ", 15, chocolate);
+    	inv.addChocolate("1");
+    	assertEquals("Chocolate is added ", 16, chocolate);
+    }
+    
+    public void testaddCoffee() throws InventoryException {
+    	inv.addCoffee("0");   	
+    	assertEquals("Coffee is 15 ", 15, inv.getCoffee());
+    	inv.addCoffee("-1");
+    	assertEquals("Negative coffee is not added ", 15, inv.getCoffee());
+    	inv.addCoffee("1");
+    	assertEquals("Coffee is added ", 16, inv.getCoffee());
+    }
+    
+    public void testaddMilk() throws InventoryException {
+    	inv.addMilk("0");   	
+    	assertEquals("Milk is 15 ", 15, milk);
+    	inv.addMilk("-1");
+    	assertEquals("Negative milk is not added ", 15, milk);
+    	inv.addMilk("1");
+    	assertEquals("Milk is added ", 16, milk);
+    }
+    
+    public void testaddSugar() throws InventoryException {
+    	inv.addSugar("0");   	
+    	assertEquals("Sugar is 15 ", 15, sugar);
+    	inv.addSugar("-1");
+    	assertEquals("Negative sugar is not added ", 15, sugar);
+    	inv.addSugar("1");
+    	assertEquals("Sugar is added ", 16, sugar);
+    }
+    
+
+ /*   
+    public void testenoughIngredience() {
+    	inv.enoughIngredients(r);
+    	
+    }
+*/
 
 }
